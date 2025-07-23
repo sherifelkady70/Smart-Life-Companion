7f97da360088e55f4d8455f475ad73423a834fe1
sherif Abdulrahman <sherifabdulrahman44@gmail.com>
2025-07-23 14:19:25 +0300

    add Ktor dependencies.

--- data/news/build.gradle.kts
+++ data/news/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -40,4 +43,40 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+
+    implementation(libs.androidx.navigation.compose)
+
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Optional: For ViewModel support
+    kapt(libs.androidx.hilt.compiler)
+
+    // Room runtime
+    implementation("androidx.room:room-runtime:2.6.1")
+
+    // Kotlin extensions and coroutines support
+    implementation("androidx.room:room-ktx:2.7.2")
+
+    // Annotation processor (for Java)
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    //coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+//
+    implementation(libs.ktor.client.core) // Ktor-Core
+    implementation(libs.ktor.client.android) // Ktor-Engine
+
+    implementation(libs.kotlinx.serialization.json) // KotlinX Serialization (Convert JSON response to Kotlin Objects)
+    implementation(libs.ktor.serialization.kotlinx.json) // Ktor- To work with Serialization
+
+    implementation(libs.ktor.client.logging) // Logging (Optional)
+
+    implementation(libs.ktor.client.content.negotiation) // Serialization
+
 }
